32740bb4f27b47c5d094171814071fa5bae60bb2
KJE-98 <>
2021-10-01 18:51:57 -0400

    made function OrthonormalBasis work

--- PCA.js
+++ PCA.js
@@ -1,5 +1,31 @@
 let numbers = require('numbers');
 
+//reflectionVector has norm 1
+function housholderReflection(reflectionVector, vector){
+  result = numbers.matrix.subtraction(
+    [reflectionVector], numbers.matrix.scalar([vector],2*numbers.matrix.dotproduct(reflectionVector,vector))
+  );
+  return result[0];
+}
+
+// $matrix is a array of basis vectors
+// $vector is the vector with coordinates cooresponding to the basis basisVectors
+// in a sense the inverse of $transform()
+function matrixVectorMultiply(matrix, vector){
+  let dimension = matrix[0].length;
+  let result = new Array(dimension).fill(0);
+  for (let i = 0; i<vector.length; i++){
+    let vectorEntry = vector[i];
+    let currentRow = matrix[i];
+    let entry = 0;
+    for (let j = 0; j<dimension; j++){
+      result[j] += currentRow[j]*vectorEntry;
+    }
+  }
+  return result;
+}
+
+
 // matrix is an array of arrays, each inner array should satisfy array.length = dimension
 // SHOULD return an array containing the mean of each dimension (average vector basically)
 function findMeans (dataset, dimension) {
@@ -31,13 +57,21 @@ function findVariance (dataset, dimension, meanVector){
 
 // matrix is an array of arrays, each inner array should satisfy array.length = dimension
 // SHOULD return the normalized and standardized dataset, and SHOULD not change the original dataset.
-function normalizeMatrix (dataset, dimension) {
+function normalizeMatrix (dataset, dimension, toStandardize) {
   let meanVector = findMeans(dataset, dimension);
-  let varianceVector = findVariance(dataset, dimension, meanVector);
   let result = new Array(dataset.length).fill().map(() => new Array(dimension).fill(0));
-  for (let i = 0; i<dataset.length; i++){
-    for (let j = 0; j<dimension; j++){
-      result[i][j] = (dataset[i][j]-meanVector[j])/varianceVector[j];
+  if (toStandardize){
+    let varianceVector = findVariance(dataset, dimension, meanVector);
+    for (let i = 0; i<dataset.length; i++){
+      for (let j = 0; j<dimension; j++){
+        result[i][j] = (dataset[i][j]-meanVector[j])/varianceVector[j];
+      }
+    }
+  }else{
+    for (let i = 0; i<dataset.length; i++){
+      for (let j = 0; j<dimension; j++){
+        result[i][j] = (dataset[i][j]-meanVector[j])
+      }
     }
   }
   return result;
@@ -48,10 +82,11 @@ function computeCovariance (dataset, dimension) {
   let result = new Array(dimension).fill().map(() => new Array(dimension).fill(0));
   console.log("created array of zeros for covariance matrix");
   for (let i = 0; i<dimension; i++){
-    console.log("computeCovariance: i index is", i);
-    for (let j = 0; j<dimension; j++){
-      for (let k = 0; k<dataset.length; k++){
-        result[i][j] += dataset[k][i]*dataset[k][j];
+    let resultRowi = result[i];
+    for (let k = 0; k<dataset.length; k++){
+      let datasetRowk = dataset[k];
+      for (let j = 0; j<dimension; j++){
+        resultRowi[j] += datasetRowk[i]*datasetRowk[j];
       }
     }
   }
@@ -60,26 +95,24 @@ function computeCovariance (dataset, dimension) {
 }
 
 // used in $computeEigenvalues()$, does not change value of $vector$
-//
 function powerIteration (vector, covariance, iterations) {
   let dimension = vector.length;
-  vector = [vector];
   for (let i = 0; i<iterations; i++){
-    let temporaryVector = numbers.matrix.multiply(vector, covariance);
-    let norm = Math.sqrt(numbers.matrix.dotproduct(temporaryVector[0],temporaryVector[0]));
+    let temporaryVector = matrixVectorMultiply(covariance, vector);
+    let norm = Math.sqrt(numbers.matrix.dotproduct(temporaryVector,temporaryVector));
 
-    temporaryVector = numbers.matrix.scalar(temporaryVector, 1/norm);
-    console.log("vector", temporaryVector, "powerIteration round", i);
-    vector = temporaryVector;
+    temporaryVector = numbers.matrix.scalar([temporaryVector], 1/norm);
+    console.log("powerIteration round", i);
+    vector = temporaryVector[0];
 
   }
 
-  return vector[0];
+  return vector;
 }
 
 // dataset in this case must already be normalized and standardized
 // given a covariance matrix SHOULD compute the the first n eigenvectors and their corresponding eigenvalues
-// DONT USE THIS FUNCTION IT DOESNT DO ANYTHING
+// DONT USE THIS FUNCTION it isnt finished yet
 function computeEigenvalues (covariance, iterations) {
   let dimension = covariance.length;
   let basisArray = new Array(dimension).fill().map(() => new Array(dimension).fill(0));
@@ -96,21 +129,19 @@ function computeEigenvalues (covariance, iterations) {
 
 function specialPowerIteration (vector, covariance, iterations, bound) {
   let dimension = vector.length;
-  vector = [vector];
   for (let i = 0; i<iterations; i++){
-    let temporaryVector = numbers.matrix.multiply(vector, covariance);
-    temporaryVector = numbers.matrix.subtraction(numbers.matrix.scalar(vector, bound),temporaryVector);
+    let temporaryVector = matrixVectorMultiply(covariance, vector);
+    temporaryVector = (numbers.matrix.subtraction(numbers.matrix.scalar([vector], bound),[temporaryVector]))[0];
 
-    let norm = Math.sqrt(numbers.matrix.dotproduct(temporaryVector[0],temporaryVector[0]));
-    temporaryVector = numbers.matrix.scalar(temporaryVector, 1/norm);
+    let norm = Math.sqrt(numbers.matrix.dotproduct(temporaryVector,temporaryVector));
+    temporaryVector = numbers.matrix.scalar([temporaryVector], 1/norm);
 
-    vector = temporaryVector;
+    vector = temporaryVector[0];
 
     console.log("specialPowerIteration: on iteration", i);
-    console.log("specialPowerIteration: current vector", vector);
   }
 
-  return vector[0];
+  return vector;
 }
 
 //must be a square matrix
@@ -130,47 +161,58 @@ function computeSmallestEigenvalue(covariance, iterations){
   let dimension = covariance.length;
   let tempVector = createRandomVector(dimension);
   let largestEigenvector = powerIteration(tempVector, covariance, iterations);
-  let largestEigenvectorScaled = numbers.matrix.multiply([largestEigenvector], covariance)[0];
-  console.log("largestEigenvectorScaled", largestEigenvectorScaled);
+  let largestEigenvectorScaled = matrixVectorMultiply(covariance, largestEigenvector);
+  //console.log("largestEigenvectorScaled", largestEigenvectorScaled);
   let largestEigenvalue = Math.sqrt(numbers.matrix.dotproduct(largestEigenvectorScaled,largestEigenvectorScaled));
-  console.log("computeSmallestEigenvalue: largest eigenvalue found- equal to ", largestEigenvalue);
+  //console.log("computeSmallestEigenvalue: largest eigenvalue found- equal to ", largestEigenvalue);
   let vector = createRandomVector(dimension);
-  console.log("createRandomVector");
+  //console.log("createRandomVector");
   // first we find a vector that moves a lot on its first go;
   return specialPowerIteration(vector, covariance, iterations, 2*largestEigenvalue);
 }
 
 // takes a direction and calculates an OrthonormalBasis for the hyperplane orthogonal to it
-function orthonormalBasis(vector){
-  let dimension = vector.length;
-  /*let basis = [vector];
-  for (let i = 1; i<dimension; i++){
-    let randomVector = [createRandomVector(dimension)];
-    for (basisVector of basis){
-      let projection = numbers.matrix.scalar(basisVector, numbers.matrix.dotproduct(basisVector[0], randomVector[0]));
-      randomVector = numbers.matrix.subtraction(randomVector, projection);
-      let norm = Math.sqrt(numbers.matrix.dotproduct(randomVector[0],randomVector[0]));
-      randomVector = numbers.matrix.scalar(randomVector, 1/norm);
-    }
-    console.log("orthonormalBasis: iteration", i);
-    basis.push(randomVector);
-  }*/
-  let tempVector = vector.map((element, index, vector) => {
-    if (index = 0){return element - Math.sqrt(numbers.matrix.dotproduct(vector,vector))} else {
+function orthonormalBasis(vector, originalBasis){
+  /*let dimension = vector.length;
+  let originalDimension = originalBasis[0][0].length;
+  //calculate v in original co-ordinatess
+  let vectorInOriginalCoordinates = matrixVectorMultiply(originalBasis.map((element) => element[0]), vector);
+  console.log("orthonormalBasis: vectorInOriginalCoordinates has length", vectorInOriginalCoordinates.length);
+  // calculate w = v - norm(v)*e_1
+  let tempVector = vectorInOriginalCoordinates.map((element, index, vector) => {
+    if (index === 0){return element - Math.sqrt(numbers.matrix.dotproduct(vector[0],vector[0]))} else {
       return element;
     }
   });
-  //calculation
+  //calculation of new basis
   let denominator = numbers.matrix.dotproduct(tempVector, tempVector);
   let numerator = numbers.matrix.multiply(numbers.matrix.transpose([tempVector]),[tempVector]);
   console.log("orthonormalBasis: outer product of tempVector with itself calculated")
-  let identity = numbers.matrix.identity(dimension);
+  let identity = originalBasis;
   let tempBasis = numbers.matrix.subtraction(
     identity,
     numbers.matrix.scalar(numerator,1/denominator)
   );
+  console.log("basis calculated");
   let basis = tempBasis.map((element) => {return [element]});
-  return basis;
+  console.log("function orthonormalBasis is returning an array of", basis.length, "arrays of length", basis[0][0].length);
+  return basis;*/
+
+  //calculate v in original co-ordinatess
+  let vectorInOriginalCoordinates = matrixVectorMultiply(originalBasis.map((element) => element[0]), vector);
+  // calculate w = v - norm(v)*e_1
+  let tempVector = vectorInOriginalCoordinates.map((element, index, vector) => {
+    if (index === 0){return element - Math.sqrt(numbers.matrix.dotproduct(vector[0],vector[0]))} else {
+      return element;
+    }
+  });
+  let denominator = Math.sqrt(numbers.matrix.dotproduct(tempVector, tempVector));
+  tempVector = numbers.matrix.scalar([tempVector], 1/denominator)[0];
+  let result = originalBasis.map(
+    (element, index) => {console.log("index", index);return [housholderReflection(tempVector, element[0])];}
+  );
+  console.log("function orthonormalBasis is returning an array of", result.length, "arrays of length", result[0][0].length);
+  return result
 }
 
 // takes m dimensional data and n normalized vectors and gives coordinates of
@@ -189,6 +231,8 @@ function transformDataMatrix(data, basisVectors){
 // takes a dataset, and basis vectors of some subpace to project onto
 
 function transform(vector, basisVectors){
+  console.log(vector.length);
+  console.log(basisVectors[1][0].length);
   let result = new Array(basisVectors.length).fill().map(
     (element,index) => numbers.matrix.dotproduct(vector, basisVectors[index][0]));
 
@@ -196,22 +240,17 @@ function transform(vector, basisVectors){
   return result;
 }
 
-function filter(dataset, basisVectors, toNormalize){
+function filter(dataset, basisVectors, toStandardize){
   dimension = basisVectors.length;
   let transformedData = dataset.map((element) => transform(element, basisVectors));
   console.log("transformedData initialized");
-  let covarianceMatrix;
-  if (toNormalize){
-    normalizedData = normalizeMatrix(transformedData, dimension);
-    console.log("normalizedData initialized");
-    covarianceMatrix = computeCovariance(normalizedData, dimension);
-    console.log("covarianceMatrix initialized:", covarianceMatrix);
-  }else{
-    covarianceMatrix = computeCovariance(transformedData, dimension);
-  }
-  let smallestComponent = computeSmallestEigenvalue(covarianceMatrix, 10);
+  normalizedData = normalizeMatrix(transformedData, dimension, toStandardize);
+  console.log("normalizedData initialized");
+  let covarianceMatrix = computeCovariance(normalizedData, dimension);
+  console.log("covarianceMatrix initialized:", covarianceMatrix);
+  let smallestComponent = computeSmallestEigenvalue(covarianceMatrix, 30);
   console.log("smallestComponent initialized");
-  let newBasis = orthonormalBasis(smallestComponent);
+  let newBasis = orthonormalBasis(smallestComponent, basisVectors);
   console.log("newBasis initialized");
   return [smallestComponent, newBasis];
 }
@@ -222,3 +261,4 @@ exports.computeSmallestEigenvalue = computeSmallestEigenvalue;
 exports.orthonormalBasis = orthonormalBasis;
 exports.transformDataMatrix = transformDataMatrix;
 exports.filter = filter;
+exports.transform = transform;
--- main.js
+++ main.js
@@ -4,67 +4,35 @@ let pgmjs = require("./pgmjs/pgmjs.js");
 let fsPromises = require("fs").promises
 
 
-/*let matrix = [[0,0,0],
-              [-1.1,-1.1,-1.15],
-              [1.05,1.15,1.1],
-              [.5,.5,.5],
-              [-.5,-.5,-.5]];
+let dataArray = [];
+let filesOuterVariable;
 
-let defaultBasis = [[[1,0,0]],
-                    [[0,1,0]],
-                    [[0,0,1]]];
-
-let filteredData = functions.filter(matrix, defaultBasis);
-//console.log('first least component', filteredData[0]);
-//console.log('first basis', filteredData[1]);
-
-let testBasis =  [
-  [ [ 0.6154836261389978, 0.13708606403288362, -0.7761361459194919 ] ],
-  [ [ 0.10545849415347704, 0.9615777103422497, 0.25346955040761354 ] ],
-  [ [ 0.7810623611246874, -0.2378565072074525, 0.5773784460935476 ] ]
-];
-
-
-let newBasis = filteredData[1].slice(1);
-
-let filteredData2 = functions.filter(matrix, newBasis);
-
-//console.log(filteredData2[0]);
-//console.log(filteredData2[1]);
-let principleComponent = numbers.matrix.multiply(filteredData2[1][1],[newBasis[0][0],newBasis[1][0]]);
-console.log('principle component', principleComponent);
-*/
-
-/// to use power iteration, find a vector that really moves on its first go
-/// could possibly do better than random vectors in the orthonormalBasis function
-let dataArray;
 let defaultBasis = numbers.matrix.identity(10304).map((element) => [element]);
 let files = fsPromises.readdir("./images");
-let array = files.then((result) => randomlyChooseFiles(result, 50));
+let array = files.then((result) => {filesOuterVariable = result;
+                                    return randomlyChooseFiles(result, 50);});
 let data = array.then((result) => Promise.all(result));
 let newData = data.then((data) => {
+  //console.log("promise data;", data);
   let filteredData = runFilter(data, defaultBasis);
-  console.log(filteredData);
+  //console.log(filteredData);
   dataArray.push(filteredData);
   return filteredData;
 });
-for (let i = 1; i < 1000; i++){
-  data = newData.then(
-    (filteredData) => {
-      let randomFiles = randomlyChooseFiles(array.state, numberOfFilesGiveni(i));
-      return randomFiles;
-    }
+for (let i = 1; i < 10304; i++){
+  array = newData.then(
+    () => {return randomlyChooseFiles(filesOuterVariable, numberOfFilesGiveni(i));}
   );
-  newData = data.then(
-    (result) => {
-      let filteredData = runFilter(data, dataArray[i-1]);
-      dataArray.push(filteredData);
-      console.log(filteredData);
-      return filteredData;
-    }
-  );
+  data = array.then((result) => Promise.all(result));
+  newData = data.then((data) => {
+    let newBasis = dataArray[i-1][1].slice(1);
+    let newData = data;
+    let filteredData = runFilter(data, newBasis, false);
+    dataArray.push(filteredData);
+    //console.log(filteredData);
+    return filteredData;
+  });
 }
-console.log("done");
 /*for (let i = 0; i<1000; i++){
   let tempArray = newData.then((result)=> randomlyChooseFiles(result))
 }*/
@@ -91,7 +59,6 @@ function randomlyChooseFiles (arrayOfFiles, numberOfFiles){
 }
 
 function runFilter(data, basis){
-  console.log("starting runFilter");
   let result = functions.filter(data, basis, false);
   return result;
 }
